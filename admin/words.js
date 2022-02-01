87d3c5b5c9b01a33c337502cef105e33d953c3f2
Chris-1965 <56934142+Chris-1965@users.noreply.github.com>
2022-02-01 22:00:02 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"fitbit adapter settings": {
+		"en": "Adapter settings for fitbit",
+		"de": "Adaptereinstellungen für fitbit",
+		"ru": "Настройки адаптера для fitbit",
+		"pt": "Configurações do adaptador para fitbit",
+		"nl": "Adapterinstellingen voor fitbit",
+		"fr": "Paramètres d'adaptateur pour fitbit",
+		"it": "Impostazioni dell'adattatore per fitbit",
+		"es": "Ajustes del adaptador para fitbit",
+		"pl": "Ustawienia adaptera dla fitbit",
+		"zh-cn": "fitbit的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
